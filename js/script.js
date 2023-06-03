dcc89bd1c469f3f96ee2f0a90321627c23f5ffaf
Halenursarisen <halenursarisen1@gmail.com>
2023-06-03 03:33:04 +0300

    düzenleme yapıldı

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let menu=document.querySelector('#menu-btn');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+document.querySelector('#login-btn').onclick = ()=>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = ()=>{
+    document.querySelector('.login-form-container').classList.remove('active');
+}
+
+
+
+
+window.onscroll = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }
+    else{
+        
+            document.querySelector('.header').classList.add('active');
+        
+    }
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+}
